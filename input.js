859c451e4c0faf1bdba05d1e9565be69847cf116
Aditya Upadhyayula <supadhy6@jhu.edu>
2022-01-20 11:18:43 -0800

    Added preloading module for all the videos

---
+++ input.js
@@ -0,0 +1,187 @@
+var stimuli = [
+{
+video_one: "0msec_047_blake_is_dead.mp4",
+video_two: "500msec_047_blake_is_dead.mp4",
+video_three: "1000msec_047_blake_is_dead.mp4"
+},
+{
+video_one:"0msec_048_the_photograph.mp4",
+video_two: "500msec_048_the_photograph.mp4",
+video_three: "1000msec_048_the_photograph.mp4"
+},
+{
+video_one:"0msec_001_summoned.mp4",
+video_two: "500msec_001_summoned.mp4",
+video_three: "1000msec_001_summoned.mp4"
+},
+{
+video_one:"0msec_002_did_they_feed_us.mp4",
+video_two: "500msec_002_did_they_feed_us.mp4",
+video_three: "1000msec_002_did_they_feed_us.mp4"
+},
+{
+video_one:"0msec_003_something_is_up.mp4",
+video_two: "500msec_003_something_is_up.mp4",
+video_three: "1000msec_003_something_is_up.mp4"
+},
+{
+video_one:"0msec_004_must_be_something_big_if_the_general_is_here.mp4",
+video_two: "500msec_004_must_be_something_big_if_the_general_is_here.mp4",
+video_three: "1000msec_004_must_be_something_big_if_the_general_is_here.mp4"
+},
+{
+video_one:"0msec_007_he_travels_the_fastest_the_one_who_travels_alone.mp4",
+video_two: "500msec_007_he_travels_the_fastest_the_one_who_travels_alone.mp4",
+video_three: "1000msec_007_he_travels_the_fastest_the_one_who_travels_alone.mp4"
+},
+{
+video_one:"0msec_008_lets_talk_about_this.mp4",
+video_two: "500msec_008_lets_talk_about_this.mp4",
+video_three: "1000msec_008_lets_talk_about_this.mp4"
+},
+{
+video_one:"0msec_009_if_we_are_not_clever_about_this_no_one_will_get_to_your_brother.mp4",
+video_two: "500msec_009_if_we_are_not_clever_about_this_no_one_will_get_to_your_brother.mp4",
+video_three: "1000msec_009_if_we_are_not_clever_about_this_no_one_will_get_to_your_brother.mp4"
+},
+{
+video_one:"0msec_010_this_is_the_frontline.mp4",
+video_two: "500msec_010_this_is_the_frontline.mp4",
+video_three: "1000msec_010_this_is_the_frontline.mp4"
+},
+{
+video_one:"0msec_011_watch_where_you_are_going.mp4",
+video_two: "500msec_011_watch_where_you_are_going.mp4",
+video_three: "1000msec_011_watch_where_you_are_going.mp4"
+},
+{
+video_one:"0msec_012_where_is_major_stevenson.mp4",
+video_two: "500msec_012_where_is_major_stevenson.mp4",
+video_three: "1000msec_012_where_is_major_stevenson.mp4"
+},
+{
+video_one:"0msec_013_we_have_orders_from_general_erinmore.mp4",
+video_two: "500msec_013_we_have_orders_from_general_erinmore.mp4",
+video_three: "1000msec_013_we_have_orders_from_general_erinmore.mp4"
+},
+{
+video_one:"0msec_014_follow_the_stench.mp4",
+video_two: "500msec_014_follow_the_stench.mp4",
+video_three: "1000msec_014_follow_the_stench.mp4"
+},
+{
+video_one:"0msec_015_age_before_beauty.mp4",
+video_two: "500msec_015_age_before_beauty.mp4",
+video_three: "1000msec_015_age_before_beauty.mp4"
+},
+{
+video_one:"0msec_016_crossing_the_front_line.mp4",
+video_two: "500msec_016_crossing_the_front_line.mp4",
+video_three: "1000msec_016_crossing_the_front_line.mp4"
+},
+{
+video_one:"0msec_017_the_injury.mp4",
+video_two: "500msec_017_the_injury.mp4",
+video_three: "1000msec_017_the_injury.mp4"
+},
+{
+video_one:"0msec_018_look_for_cover.mp4",
+video_two: "500msec_018_look_for_cover.mp4",
+video_three: "1000msec_018_look_for_cover.mp4"
+},
+{
+video_one:"0msec_019_fighter_planes_they_are_ours.mp4",
+video_two: "500msec_019_fighter_planes_they_are_ours.mp4",
+video_three: "1000msec_019_fighter_planes_they_are_ours.mp4"
+},
+{
+video_one:"0msec_020_there_is_a_gap_in_the_wire.mp4",
+video_two: "500msec_020_there_is_a_gap_in_the_wire.mp4",
+video_three: "1000msec_020_there_is_a_gap_in_the_wire.mp4"
+},
+{
+video_one:"0msec_021_that_is_their_front_line.mp4",
+video_two: "500msec_021_that_is_their_front_line.mp4",
+video_three: "1000msec_021_that_is_their_front_line.mp4"
+},
+{
+video_one:"0msec_022_they_really_are_gone.mp4",
+video_two: "500msec_022_they_really_are_gone.mp4",
+video_three: "1000msec_022_they_really_are_gone.mp4"
+},
+{
+video_one:"0msec_023_germas_are_not_long_gone.mp4",
+video_two: "500msec_023_germas_are_not_long_gone.mp4",
+video_three: "1000msec_023_germas_are_not_long_gone.mp4"
+},
+{
+video_one:"0msec_024_there_might_be_a_way_through.mp4",
+video_two: "500msec_024_there_might_be_a_way_through.mp4",
+video_three: "1000msec_024_there_might_be_a_way_through.mp4"
+},
+{
+video_one:"0msec_025_even_their_rats_are_bigger_than_ours.mp4",
+video_two: "500msec_025_even_their_rats_are_bigger_than_ours.mp4",
+video_three: "1000msec_025_even_their_rats_are_bigger_than_ours.mp4"
+},
+{
+video_one:"0msec_026_trip_wire_donot_move.mp4",
+video_two: "500msec_026_trip_wire_donot_move.mp4",
+video_three: "1000msec_026_trip_wire_donot_move.mp4"
+},
+{
+video_one:"0msec_027_wake_up_schof.mp4",
+video_two: "500msec_027_wake_up_schof.mp4",
+video_three: "1000msec_027_wake_up_schof.mp4"
+},
+{
+video_one:"0msec_028_you_have_to_jump.mp4",
+video_two: "500msec_028_you_have_to_jump.mp4",
+video_three: "1000msec_028_you_have_to_jump.mp4"
+},
+{
+video_one:"0msec_029_pick_some_one_else.mp4",
+video_two: "500msec_029_pick_some_one_else.mp4",
+video_three: "1000msec_029_pick_some_one_else.mp4"
+},
+{
+video_one:"0msec_030_do_you_want_to_go_back.mp4",
+video_two: "500msec_030_do_you_want_to_go_back.mp4",
+video_three: "1000msec_030_do_you_want_to_go_back.mp4"
+},
+{
+video_one:"0msec_031_light_the_flare.mp4",
+video_two: "500msec_031_light_the_flare.mp4",
+video_three: "1000msec_031_light_the_flare.mp4"
+},
+{
+video_one:"0msec_032_they_have_destroyed_their_own_guns.mp4",
+video_two: "500msec_032_they_have_destroyed_their_own_guns.mp4",
+video_three: "1000msec_032_they_have_destroyed_their_own_guns.mp4"
+},
+{
+video_one:"0msec_033_fighter_planes_wonder_what_They_say.mp4",
+video_two: "500msec_033_fighter_planes_wonder_what_They_say.mp4",
+video_three: "1000msec_033_fighter_planes_wonder_what_They_say.mp4"
+},
+{
+video_one:"0msec_037_the_abandoned_house.mp4",
+video_two: "500msec_037_the_abandoned_house.mp4",
+video_three: "1000msec_037_the_abandoned_house.mp4"
+},
+{
+video_one:"0msec_038_inspecting_the_abandoned_house.mp4",
+video_two: "500msec_038_inspecting_the_abandoned_house.mp4",
+video_three: "1000msec_038_inspecting_the_abandoned_house.mp4"
+},
+{
+video_one:"0msec_039_milk.mp4",
+video_two: "500msec_039_milk.mp4",
+video_three: "1000msec_039_milk.mp4"
+},
+{
+video_one:"0msec_040_is_that_our_friends_again.mp4",
+video_two: "500msec_040_is_that_our_friends_again.mp4",
+video_three: "1000msec_040_is_that_our_friends_again.mp4"
+},
+]
